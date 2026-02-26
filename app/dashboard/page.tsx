0c59640b8e5f7fff8381fc7155f5814d31c05d87
Maalik-ghub <nabeellatheef9@gmail.com>
2026-02-26 14:27:50 +0530

    implemented login feature localStorage to store login state

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,16 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Dashboard() {
+    const router = useRouter();
+    useEffect(() => {
+        const auth = localStorage.getItem("auth");
+        if(!auth) {
+          router.push("/login")
+        }
+      }, [router])
+
     return(
         <div className="h-[100vh] w-[100%]"></div>
     )
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
   Card,
   CardAction,
@@ -10,13 +11,33 @@ import {
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useRouter } from "next/navigation"
 
 export default function Login() {
+    const router = useRouter();
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        const form = e.currentTarget;
+        const formData = new FormData(form);
+        const email = formData.get("email");
+        const password = formData.get("password");
+        const auth: string | null = localStorage.getItem("auth");
+        if(!auth) {
+            const user = {
+                email: email,
+                pass: password
+            }
+            localStorage.setItem("auth", JSON.stringify(user));
+            router.push("/dashboard")
+        }
+    }
+
     return (
         <div className="h-[100vh] w-[100%] bg-zinc-900 flex justify-center items-center">
-            <Card className="w-full max-w-sm">
+            <Card className="w-[85vw] md:w-md xl:scale-[1.3]">
       <CardHeader>
-        <CardTitle>Login to your account</CardTitle>
+        <CardTitle>Login to todo account</CardTitle>
         <CardDescription>
           Enter your email below to login to your account
         </CardDescription>
@@ -24,12 +45,12 @@ export default function Login() {
         </CardAction>
       </CardHeader>
       <CardContent>
-        <form>
+        <form onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}>
           <div className="flex flex-col gap-6">
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
               <Input
-                id="email"
+                name="email"
                 type="email"
                 placeholder="m@example.com"
                 required
@@ -38,23 +59,17 @@ export default function Login() {
             <div className="grid gap-2">
               <div className="flex items-center">
                 <Label htmlFor="password">Password</Label>
-                <a
-                  href="#"
-                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                >
-                  Forgot your password?
-                </a>
               </div>
-              <Input id="password" type="password" required />
+              <Input name="password" type="password" required />
             </div>
           </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex-col gap-2">
+          <CardFooter className="flex-col gap-2 mt-[20px]">
         <Button type="submit" className="w-full">
           Login
         </Button>
       </CardFooter>
+        </form>
+      </CardContent>
     </Card>
         </div>
     )
